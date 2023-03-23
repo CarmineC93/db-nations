e35742cb160e74fe224d02069e981918e7c83449
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-03-23 15:50:55 +0100

    modify query with user filterKey

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
             String URL = "jdbc:mysql://localhost:3306/db-nations";
             String USER = "root" ;
             String PASSWORD = "root";
+
+            //chiedo all'utente una parola per la ricerca
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Insert a filter key: ");
+            String keyWord = scan.nextLine();
 
             try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)){
                 //System.out.println(conn.isClosed());
@@ -15,11 +21,16 @@ public class Main {
                         from countries c\s
                         join regions r on c.region_id = r.region_id\s
                         join continents c2 on c2.continent_id = r.continent_id
+                        where c.name like ?
                         order by c.name ;
                         """;
 
                 //la connection prepara uno statement sql dalla query in parametro
                 try(PreparedStatement ps = conn.prepareStatement(query)){
+
+                    //associo il parametro per evitare SQL Injections
+                    ps.setString(1, "%" + keyWord + "%");
+
                     //eseguo la query e il risultato va nel ResultSet
                     try(ResultSet rs = ps.executeQuery()){
                         //definisco il contenuto del ResultSet
@@ -31,8 +42,8 @@ public class Main {
 
                             System.out.println(
                                     nation_name + " " +  id + " " + region_name + " " + continent_name);
-                        }
 
+                        }
                     }
                 }
             } catch (SQLException e) {
