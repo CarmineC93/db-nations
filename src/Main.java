d65c8b2f06758d095dab29972e46d2063a0ad7be
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-03-23 15:08:34 +0100

    print the ResultSet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +7,34 @@ public class Main {
             String PASSWORD = "root";
 
             try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)){
-                System.out.println(conn.isClosed());
+                //System.out.println(conn.isClosed());
+
+                //          nome, id, nome della regione e nome del continente,
+                String query = """
+                        SELECT  c.name AS nation_name, c.country_id AS nation_id, r.name AS region_name, c2.name AS continent_name
+                        from countries c\s
+                        join regions r on c.region_id = r.region_id\s
+                        join continents c2 on c2.continent_id = r.continent_id
+                        order by c.name ;
+                        """;
+
+                //la connection prepara uno statement sql dalla query in parametro
+                try(PreparedStatement ps = conn.prepareStatement(query)){
+                    //eseguo la query e il risultato va nel ResultSet
+                    try(ResultSet rs = ps.executeQuery()){
+                        //definisco il contenuto del ResultSet
+                        while(rs.next()){
+                            String nation_name = rs.getString("nation_name");
+                            Integer id = rs.getInt("nation_id");
+                            String region_name = rs.getString("nation_name");
+                            String continent_name = rs.getString("continent_name");
+
+                            System.out.println(
+                                    nation_name + " " +  id + " " + region_name + " " + continent_name);
+                        }
+
+                    }
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
